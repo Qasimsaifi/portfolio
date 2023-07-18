b4846a4e14be4fcc068aad3a17b533137ab853d2
Qasimsaifi <106295428+Qasimsaifi@users.noreply.github.com>
2023-07-18 23:20:33 +0530

    sd

--- src/pages/store.jsx
+++ src/pages/store.jsx
@@ -1,22 +1,23 @@
-import Footer from '@/components/Footer'
-import NavBar from '@/components/Navbar'
-import Store from '@/components/Store'
-import React from 'react'
-import Head from 'next/head'
+import Footer from "@/components/Footer";
+import NavBar from "@/components/Navbar";
+import Store from "@/components/Store";
+import React from "react";
+import Head from "next/head";
 const store = () => {
-
-
   return (
     <>
-    <Head>
-      <title>Online Store - Kasim Saifi Web Products</title>
-      <meta name="description" content="Take your web development projects to the next level with our range of high-quality products. From themes and templates to plugins and tools, KASIM Store offers everything you need to enhance your web development workflow."></meta>
-    </Head>
-      <NavBar/>
-      <Store/>
-      <Footer/>
+      <Head>
+        <title>Online Store - Kasim Saifi Web Products</title>
+        <meta
+          name="description"
+          content="Take your web development projects to the next level with our range of high-quality products. From themes and templates to plugins and tools, KASIM Store offers everything you need to enhance your web development workflow."
+        ></meta>
+      </Head>
+      <NavBar />
+      <Store />
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default store
+export default store;
