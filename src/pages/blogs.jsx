ed2102372e2483f5cbca1bf477caaea22f5dff3a
Qasimsaifi <106295428+Qasimsaifi@users.noreply.github.com>
2023-07-19 12:22:34 +0530

    done

--- src/pages/blogs.jsx
+++ src/pages/blogs.jsx
@@ -9,11 +9,24 @@ const blog = () => {
     <>
       <Head>
         <title>Web Development Blog: Stay Updated with Kasim Saifi's Expert Insights and Trends</title>
-        <meta name="description" content="Explore our portfolio of successful web development projects. At KASIM, we have helped numerous clients achieve their online goals through innovative designs, seamless functionality, and user-friendly interfaces. Discover our expertise today."></meta>
+        <meta name="description" content="Explore our portfolio of successful web development projects. At KASIM, we have helped numerous clients achieve their online goals through innovative designs, seamless functionality, and user-friendly interfaces. Discover our expertise today." />
+        {/* Meta tags for social media sharing */}
+        <meta property="og:title" content="Web Development Blog: Stay Updated with Kasim Saifi's Expert Insights and Trends" />
+        <meta property="og:description" content="Explore our portfolio of successful web development projects. At KASIM, we have helped numerous clients achieve their online goals through innovative designs, seamless functionality, and user-friendly interfaces. Discover our expertise today." />
+        <meta property="og:image" content="" /> {/* Replace with the URL of your featured image */}
+        <meta property="og:url" content="URL_OF_THE_PAGE" /> {/* Replace with the URL of this specific page */}
+        <meta property="og:type" content="article" />
+
+        {/* Meta tags for Twitter sharing */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Web Development Blog: Stay Updated with Kasim Saifi's Expert Insights and Trends" />
+        <meta name="twitter:description" content="Explore our portfolio of successful web development projects. At KASIM, we have helped numerous clients achieve their online goals through innovative designs, seamless functionality, and user-friendly interfaces. Discover our expertise today." />
+        <meta name="twitter:image" content="" /> {/* Replace with the URL of your featured image */}
+
       </Head>
       <NavBar />
       <BlogsPage />
-      <Footer/>
+      <Footer />
     </>
   );
 };
