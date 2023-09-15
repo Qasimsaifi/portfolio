0f54654c120f6b4c1cb79c33cd0a3da9c6340551
Kasim Saifi <106295428+Qasimsaifi@users.noreply.github.com>
2023-09-15 17:32:12 +0530

    ested

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React from "react";
 import { Fade } from "react-awesome-reveal";
 import { Reveal } from "react-awesome-reveal";
@@ -33,39 +33,47 @@ from {
 `;
 function About() {
   return (
-    <Fade cascade damping={0.4} >
+    <Fade cascade damping={0.4}>
       <div className="py-12 mt-4 mb-8">
         <div className="container mx-auto px-4">
           <div className="max-w-2xl mx-auto text-center">
-            <Fade cascade damping={0.7} >
-              <h2 className="text-3xl font-semibold  mb-6">About Me</h2></Fade>
+            <Fade cascade damping={0.7}>
+              <h2 className="text-3xl font-semibold mb-6">About Me</h2>
+            </Fade>
             <Reveal keyframes={customFromLeftAnimation}>
               <p className="text-lg">
-                Hey there! I'm Kasim Saifi, a full-stack web developer based in the
-                vibrant city of Uttar Pradesh, India. I'm passionate about crafting
-                digital experiences that are not only beautiful but also functional.
-              </p></Reveal>
+                Hey there! I'm Kasim Saifi, and I'm more than just a web
+                developer. I'm a creative problem solver based in the
+                picturesque town of Gawan, located in Sambhal, Uttar Pradesh,
+                India.
+              </p>
+            </Reveal>
             <Reveal keyframes={customFromRightAnimation}>
               <p className="text-lg mt-4">
-                My journey in web development began with a fascination for creating
-                interactive websites. Over the years, I've honed my skills in
-                front-end technologies like HTML, CSS, and JavaScript, and I'm
-                equally comfortable working on the back end with Python and Node.js.
-              </p></Reveal>
+                My journey in web development started with a simple curiosity
+                that blossomed into a passion for crafting remarkable online
+                experiences. I specialize in front-end technologies like HTML,
+                CSS, and JavaScript, but I'm equally proficient in the back end
+                with Python and Node.js.
+              </p>
+            </Reveal>
             <Reveal keyframes={customFromLeftAnimation}>
               <p className="text-lg mt-4">
-                When I'm not coding, you can find me exploring the latest trends in
-                web development, experimenting with new frameworks and libraries, or
-                seeking inspiration from design and tech communities. I believe in
-                the power of lifelong learning, and I'm always excited to take on
-                new challenges.
-              </p></Reveal>
+                When I'm not immersed in code, you'll find me exploring the
+                ever-evolving landscape of web development, experimenting with
+                new tools and techniques, and finding inspiration in the dynamic
+                worlds of design and technology. Lifelong learning is not just a
+                motto for me; it's a way of life.
+              </p>
+            </Reveal>
             <Reveal keyframes={customFromRightAnimation}>
               <p className="text-lg mt-4">
-                I'm here to bring your ideas to life and create web applications
-                that leave a lasting impression. Let's collaborate and turn your
-                vision into reality!
-              </p></Reveal>
+                As a passionate developer, I thrive on the excitement of turning
+                ideas into reality. My expertise lies in creating web
+                applications that are not only functional but also visually
+                stunning. Let's collaborate and bring your vision to life!
+              </p>
+            </Reveal>
             <div className="mt-8">
               <a href="/contact">
                 <button className="btn">Contact Me</button>
